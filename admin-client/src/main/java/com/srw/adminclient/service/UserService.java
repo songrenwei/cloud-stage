26dd833df68f661464d0b0d8b6570b531cf3fa87
songrenwei <songrenwei@ehousechina.com>
2020-12-01 16:44:21 +0800

    整合SpringBoot Admin

---
+++ admin-client/src/main/java/com/srw/adminclient/service/UserService.java
@@ -0,0 +1,23 @@
+package com.srw.adminclient.service;
+
+
+import com.srw.adminclient.domain.User;
+
+import java.util.List;
+
+/**
+ * Created by macro on 2019/8/29.
+ */
+public interface UserService {
+    void create(User user);
+
+    User getUser(Long id);
+
+    void update(User user);
+
+    void delete(Long id);
+
+    User getByUsername(String username);
+
+    List<User> getUserByIds(List<Long> ids);
+}
---
+++ admin-server-security/src/test/java/com/srw/adminserversecurity/AdminServerSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srw.adminserversecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
